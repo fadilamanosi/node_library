4eb93b92518bcd7d7841fb7fec86b987ed3a95d7
fadilamanosi <fadilamanosi@gmail.com>
2021-09-22 11:58:26 +0000

    .

---
+++ build/crypto.d.ts
@@ -0,0 +1,3 @@
+declare const encrypt: (text: string) => string;
+declare const decrypt: (hash: string) => any;
+export { decrypt, encrypt };
---
+++ build/crypto.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.encrypt = exports.decrypt = void 0;
+const crypto = require('crypto');
+const algorithm = 'aes-256-ctr';
+const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
+const iv = crypto.randomBytes(16);
+const encrypt = (text) => {
+    const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+    return JSON.stringify({
+        iv: iv.toString('hex'),
+        content: encrypted.toString('hex')
+    });
+};
+exports.encrypt = encrypt;
+const decrypt = (hash) => {
+    var hashed = JSON.parse(hash);
+    const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(hashed.iv, 'hex'));
+    const decrpyted = Buffer.concat([decipher.update(Buffer.from(hashed.content, 'hex')), decipher.final()]);
+    return decrpyted;
+};
+exports.decrypt = decrypt;
